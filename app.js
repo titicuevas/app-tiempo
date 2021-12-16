b6e8364c72e1f94488a491944cd774f43df93ebc
Enrique Cuevas Garcia <enrique.cuevas@iesdonana.org>
2021-12-16 16:00:46 +0100

    Salen los contenidos pasados por js

---
+++ app.js
@@ -0,0 +1,37 @@
+const API_KEY = "e0bc2f18ba1cbeef7329aeb1f896ae47";
+
+const fetchData = (position) => {
+  const { latitude, longitude } = position.coords;
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((data) => setWeatherData(data));
+};
+
+const setWeatherData = (data) => {
+  console.log(data);
+  const weatherData = {
+    location: data.name,
+    description: data.weather[0].main,
+    humidity: data.main.humidity,
+    pressure: data.main.pressure,
+    temperature: data.main.temp,
+    date: getDate(),
+  };
+
+  Object.keys(weatherData).forEach((key) => {
+    document.getElementById(key).textContent = weatherData[key];
+  });
+};
+
+const getDate = () => {
+  let date = new Date();
+
+  return `${date.getDate()}-${(
+    '0' + (date.getMonth() + 1)).slice(-2)} -${date.getFullYear()}`;
+};
+
+const onLoad = () => {
+  navigator.geolocation.getCurrentPosition(fetchData);
+};
